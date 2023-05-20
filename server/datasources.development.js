ca6f26988536685237aa33e0d6a4bcfaff1cb56a
latjoh <latjo205@gmail.com>
2023-05-20 14:13:15 -0400

    Latest

--- server/datasources.development.js
+++ server/datasources.development.js
@@ -3,8 +3,8 @@ module.exports = {
    connector: 'mongodb',
    hostname: process.env.DB_HOST ||'localhost',
    port: process.env.DB_PORT ||27017,
-   user: process.env.DB_USER ||",
-   password: process.env.DB_PASSWORD ||",
+   user: process.env.DB_USER ||"",
+   password: process.env.DB_PASSWORD ||"",
    database: process.env.DB_NAME ||'lafs',
    url: process.env.DB_URL
  }
